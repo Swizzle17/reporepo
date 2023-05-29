2d001e3ede9ea54df1238870ee58d3e1196a6f8c
Dwi Sulistiyani <dwisulistiiyani20@gmail.com>
2023-05-29 13:32:52 +0700

    push

---
+++ java01syntax.java
@@ -0,0 +1,27 @@
+public class java01syntax {
+
+	public static void main(String[] args) {
+		/*
+		 * Logic
+		 */
+
+		int i = 1;
+		// CTRL + SPACE
+		System.out.println("test execution starts here.."); // Logs
+
+		String D = "Hey its me Dwi";
+		System.out.println("Test Word: " + D);
+		System.out.println(+i);
+
+		/*
+		 * Logic
+		 */
+		if (D.startsWith("Dwi")) {
+			System.out.println("test passed");
+		} else {
+			System.out.println("test failed");
+		}
+
+	}
+
+}
